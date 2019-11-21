87271d923e3c4ce5d02d305cad7d7c8bccac9700
Nargiz <7788342@mail.ru>
2019-11-22 02:11:44 +0300

    Lesson 1 22.11.19

--- module10/1/FileExperiments/src/Loader.java
+++ module10/1/FileExperiments/src/Loader.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Loader {
     public static void main(String[] args) {
-        System.out.println("Введите директорию: ");
+        System.out.println("Enter directory: ");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine().trim();
         //path = "C:/Users/Наргиз/Desktop/JavaProjects0Pro1/MyFirstProject";
@@ -16,17 +16,35 @@ public class Loader {
     private static void getContents(File directory, int n) {
         File[] files = directory.listFiles();
         n++;
-        if(files != null) {
+        if (files != null) {
             for (File file : files) {
-                for (int i = 1; i < n; i++) System.out.print("  ");
+                for (int i = 1; i < n; i++) {
+                    System.out.print("  ");
+                }
                 if (file.isFile()) {
-                    System.out.println(file.getName() + " " + file.length());
+                    System.out.println(file.getName() + " (" + file.length() + " bytes)");
                 }
                 if (file.isDirectory()) {
-                    System.out.println(file.getName() + " " + Objects.requireNonNull(file.listFiles()).length);
+                    System.out.println(file.getName() + " (" + directorySize(file) + " bytes)");
                     getContents(file, n);
                 }
             }
         }
+    }
+
+    private static long directorySize(File directory) {
+        long length = 0;
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    length += file.length();
+                }
+                if (file.isDirectory()) {
+                    length += directorySize(file);
+                }
+            }
+        }
+        return length;
     }
 }
