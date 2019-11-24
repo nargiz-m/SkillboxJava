83b1eaf7ab826c405cdfe8c9ee1c9a2e523f828e
Nargiz <7788342@mail.ru>
2019-11-24 20:52:28 +0300

    Lesson 3 24.11.2019

--- module10/3/Probabilities/src/Loader.java
+++ module10/3/Probabilities/src/Loader.java
@@ -2,63 +2,66 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Loader {
-    public static void main(String[] args) throws IOException {
-        String src = "res\\Probabilites.txt";
-        String dest = "res\\formatted.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(src));
+    public static void main(String[] args) {
+        Path src = Paths.get("res", "Probabilites.txt");
+        Path dest = Paths.get("res", "formatted.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(src.toString()));
+            HashMap<String, Integer> name = new HashMap<>();
+            PrintWriter pw = new PrintWriter(dest.toString());
+            pw.write("\t\t");
 
-        HashMap<String, Integer> name = new HashMap<>();
-        HashMap<String, Integer> date = new HashMap<>();
-        String[][] objects = new String[100][100];
+            int length = 0;
 
-        int length = 0;
-
-        for(int i = 0;;i++) {
-            String line = reader.readLine();
-            if(line == null) {
-                break;
-            }
-            String[] currentRow = line.trim().split("\\s+");
-            if(i == 0) {
-                for(int j = 0; j < currentRow.length; j++) name.put(currentRow[j], j + 1);
-            }
-            else {
-                date.put(currentRow[0], i);
-                for(int j = 1; j < currentRow.length; j++) {
-                    objects[i][j] = currentRow[j];
-                    if(objects[i][j].length() > length) {
-                        length = objects[i][j].length();
+            for (int i = 0; ; i++) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] currentRow = line.trim().split("\\s+");
+                if (i == 0) {
+                    for (int j = 0; j < currentRow.length; j++) {
+                        name.put(currentRow[j], j + 1);
+                        if (currentRow[j].length() > length) {
+                            length = currentRow[j].length();
+                        }
+                    }
+                    name = sortByValue(name);
+                    for (String nameInstance : name.keySet()) {
+                        String strToPrint = "\t" + nameInstance;
+                        for (int j = 0; j < (length - nameInstance.length()) / 4; j++) strToPrint += "\t";
+                        pw.write("\t|" + strToPrint);
+                    }
+                } else {
+                    for (int j = 1; j < currentRow.length; j++) {
+                        if (currentRow[j].length() > length) {
+                            length = currentRow[j].length();
+                        }
+                    }
+                    pw.write("\n" + currentRow[0]);
+                    for (int j = 1; j < currentRow.length; j++) {
+                        String strToPrint = "\t" + currentRow[j];
+                        for (int k = 0; k < (length - currentRow[j].length()) / 4; j++) {
+                            strToPrint += "\t";
+                        }
+                        pw.write("\t|" + strToPrint);
                     }
                 }
             }
-        }
-        name = sortByValue(name);
-        date = sortByValue(date);
 
-        PrintWriter pw = new PrintWriter(dest);
-        pw.write("\t\t");
-        for (String nameInstance : name.keySet()) {
-            String strToPrint = "\t" + nameInstance;
-            for(int j = 0; j < (length - nameInstance.length())/4; j++) strToPrint += "\t";
-            pw.write("\t|" + strToPrint);
+            pw.flush();
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        for(String dateInstance : date.keySet()) {
-            pw.write("\n" + dateInstance);
-            for(int i = 1; i <= name.size(); i++) {
-                String strToPrint = "\t" + objects[date.get(dateInstance)][i];
-                for(int j = 0; j < (length - objects[date.get(dateInstance)][i].length())/4; j++) strToPrint += "\t";
-                pw.write("\t|" + strToPrint);
-            }
-        }
-        pw.flush();
-        pw.close();
     }
 
-    private static HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsorted){
+    private static HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsorted) {
         List<Map.Entry<String, Integer>> list = new LinkedList<>(unsorted.entrySet());
         list.sort(new Comparator<Map.Entry<String, Integer>>() {
             @Override
