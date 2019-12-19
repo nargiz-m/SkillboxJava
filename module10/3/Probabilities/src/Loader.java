15b47bde624d601ca9840e36492deee208db425a
Nargiz <7788342@mail.ru>
2019-12-20 00:47:06 +0300

    lesson 3 20.12/2019

--- module10/3/Probabilities/src/Loader.java
+++ module10/3/Probabilities/src/Loader.java
@@ -1,64 +1,54 @@
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Loader {
     public static void main(String[] args) {
         Path src = Paths.get("res", "Probabilites.txt");
         Path dest = Paths.get("res", "formatted.txt");
 
-        try (FileReader fileReader = new FileReader(src.toString());
+        try (FileReader fileReader = new FileReader(src.toString(), Charset.forName("windows-1251"));
              BufferedReader reader = new BufferedReader(fileReader);
              PrintWriter pw = new PrintWriter(dest.toString()))
         {
-            HashMap<String, Integer> name = new HashMap<>();
             pw.write("\t\t");
-
-            int length = 0;
-
             for (int i = 0; ; i++) {
                 String line = reader.readLine();
                 if (line == null) {
                     break;
                 }
+
                 String[] currentRow = line.trim().split("\\s+");
-                if (i == 0) {
-                    for (int j = 0; j < currentRow.length; j++) {
-                        name.put(currentRow[j], j + 1);
-                        if (currentRow[j].length() > length) {
-                            length = currentRow[j].length();
-                        }
-                    }
-                    name = sortByValue(name);
-                    for (String nameInstance : name.keySet()) {
-                        StringBuilder strToWrite = new StringBuilder();
-                        strToWrite.append("\t" + nameInstance);
-                        for (int j = 0; j < (length - nameInstance.length()) / 4; j++) {
-                            strToWrite.append("\t");
-                        }
-                        pw.write("\t|" + strToWrite);
-                    }
-                } else {
-                    for (int j = 1; j < currentRow.length; j++) {
-                        if (currentRow[j].length() > length) {
-                            length = currentRow[j].length();
-                        }
-                    }
-                    pw.write("\n" + currentRow[0]);
-                    for (int j = 1; j < currentRow.length; j++) {
-                        StringBuilder strToWrite = new StringBuilder();
-                        strToWrite.append("\t" + currentRow[j]);
-                        for (int k = 0; k < (length - currentRow[j].length()) / 4; j++) {
-                            strToWrite.append("\t");
-                        }
-                        pw.write("\t|" + strToWrite);
-                    }
-                }
+                pw.write(writeFormattedRow(currentRow, i) + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static StringBuilder writeFormattedRow(String[] currentRow, Integer rowType){
+        int length = 0;
+        StringBuilder strToWrite = new StringBuilder();
+        int start = rowType == 0 ? 0 : 1;
+        for (int j = start; j < currentRow.length; j++) {
+            if (currentRow[j].length() > length) {
+                length = currentRow[j].length();
+            }
+        }
+        if (start == 1) strToWrite.append(currentRow[0]);
+        for (int j = start; j < currentRow.length; j++) {
+            StringBuilder currentValue = new StringBuilder();
+            currentValue.append("\t").append(currentRow[j]);
+            for (int k = 0; k < (length - currentRow[j].length()) / 4; k++) {
+                currentValue.append("\t");
+            }
+            strToWrite.append("\t|").append(currentValue);
+        }
+        return strToWrite;
     }
 
     private static HashMap<String, Integer> sortByValue(HashMap<String, Integer> unsorted) {
