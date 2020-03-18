6b997d9ecbc90e0e012f14744c477203609909d7
Nargiz <7788342@mail.ru>
2020-03-18 20:29:22 +0300

    Otrezki with List

--- module11/Otrezki/src/Otrezki.java
+++ module11/Otrezki/src/Otrezki.java
@@ -1,43 +1,93 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Otrezki {
 
     public static class Interval {
         public final int from;
         public final int to;
+        public final String name;
 
-        public Interval(int from, int to) {
+        public Interval(int from, int to, String name) {
             this.from = from;
             this.to = to;
+            this.name = name;
         }
 
         @Override
         public String toString() {
-            return String.format("[%d-%d]", from, to);
+            return String.format("[%2d-%2d] (%s)", from, to, name);
         }
     }
+
     public static void main(String[] args) {
-        String str1 = "a-----b";
-        String str2 = "   c-----d";
+        String str =
+           "     a-----a\n"             +
+           "  b-----b\n"                +
+           "          c-----c\n"        +
+           "                   d-d\n"   +
+           "e---------------------e\n"  +
+           "                          f---f\n";
+        List<Interval> intervals = Arrays.stream(str.split("\n"))
+                .map(Otrezki::fromRow)
+                .collect(Collectors.toList());
 
-        Interval i1 = Otrezki.fromRow(str1);
-        Interval i2 = Otrezki.fromRow(str2);
+        System.out.println("Имеются отрезки:");
+        intervals.forEach(System.out::println);
 
-        System.out.println("i1: " + str1);
-        System.out.println("i2: " + str2);
-        System.out.println("intersect? " + isIntersect(i1, i2));
+        System.out.println("");
+        System.out.println("Из них пересекаются:");
+        findIntersections(intervals).forEach(group -> {
+            System.out.printf(" - %s",
+                    group.stream().map(i -> i.name).collect(Collectors.joining(",")));
+            System.out.println();
+        });
+
+        // Для примера выше:
+        // Из них пересекаются (вариант ответа 1):
+        // a,b
+        // a,e
+        // a,c
+        // b,a
+        // b,e
+        // c,e
+        // d,e
+
+        // Из них пересекаются (вариант ответа 2):
+        // a,b,e
+        // a,c,e
+        // d,e
+    }
+
+    public static List<List<Interval>> findIntersections(List<Interval> intervals) {
+        List<List<Interval>> intersections = new ArrayList<>();
+        for (int i = 0; i < intervals.size(); i++) {
+            for(int j = i + 1; j < intervals.size(); j++) {
+                if(isIntersect(intervals.get(i), intervals.get(j))) {
+                    intersections.add(new ArrayList<>(Arrays.asList(intervals.get(i), intervals.get(j))));
+                }
+            }
+        }
+        return intersections;
     }
 
 
     public static boolean isIntersect(Interval i1, Interval i2) {
-        return (i1.from < i2.from && i2.from < i1.to) || (i2.from < i1.to && i1.from < i2.to);
+        return !(i1.from >= i2.to || i2.from >= i1.to);
     }
 
     public static Interval fromRow(String str) {
-        var firstSymbol = Pattern.compile("^(\\s*[a-z])").matcher(str);
+        var firstSymbol = Pattern.compile("^\\s*([a-z])").matcher(str);
         var lastSymbol = Pattern.compile("^(.*[a-z])\\s*$").matcher(str);
         if (firstSymbol.find() && lastSymbol.find()) {
-            return new Interval(firstSymbol.group().length() - 1, lastSymbol.group(1).length() - 1);
+            return new Interval(
+                    firstSymbol.group().length() - 1,
+                    lastSymbol.group(1).length() - 1,
+                    firstSymbol.group(1));
         }
         throw new RuntimeException("Bad format of string: " + str);
     }
